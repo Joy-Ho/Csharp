877c79fc99eb0a9e5258636401307cbdb6659bb6
Joy-Ho <joy851025@gmail.com>
2024-11-17 21:58:00 +0800

    initial commit

---
+++ CountCharacterOfWord/CountCharacterOfWord/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a word: ");
+String word = Console.ReadLine();
+
+if (word.Length < 5)
+{
+    Console.WriteLine(word + " is less than 5 characters.");
+}
+else if(word.Length < 11)
+{
+    Console.WriteLine(word + " is between 5 and 10 characters.");
+}
+else
+{
+    Console.WriteLine(word + " is larger than 10 characters.");
+}
